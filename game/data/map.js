fba7c61397547f3d81cfa3f68bd38b67432faeae
Matias Arriola <matias.arriola@gmail.com>
2014-08-25 14:59:22 -0300

    map update

--- game/data/map.js
+++ game/data/map.js
@@ -23,8 +23,8 @@ module.exports = [
         "y": 240
       },
       {
-        "x": 690,
-        "y": 95
+        "x": 710,
+        "y": 205
       }
     ]
   },
@@ -107,6 +107,18 @@ module.exports = [
       {
         "x": -790,
         "y": 665
+      },
+      {
+        "x": -445,
+        "y": -1280
+      },
+      {
+        "x": -265,
+        "y": -1185
+      },
+      {
+        "x": -105,
+        "y": -1125
       }
     ]
   },
@@ -118,7 +130,339 @@ module.exports = [
     "target": {
       "x": 1935,
       "y": -375
-    }
+    },
+    "elements": [
+      {
+        "type": "obstacle:cup",
+        "pos": {
+          "x": 1655,
+          "y": 515
+        }
+      },
+      {
+        "type": "obstacle:pillow",
+        "pos": {
+          "x": -550,
+          "y": 1450
+        }
+      },
+      {
+        "type": "obstacle:pillow",
+        "pos": {
+          "x": -695,
+          "y": 1405
+        }
+      },
+      {
+        "type": "obstacle:pillow",
+        "pos": {
+          "x": -810,
+          "y": 1325
+        }
+      },
+      {
+        "type": "obstacle:clock",
+        "pos": {
+          "x": -1530,
+          "y": 845
+        }
+      },
+      {
+        "type": "obstacle:clock",
+        "pos": {
+          "x": -1785,
+          "y": 430
+        }
+      },
+      {
+        "type": "obstacle:clock",
+        "pos": {
+          "x": -1825,
+          "y": 180
+        }
+      },
+      {
+        "type": "obstacle:clock",
+        "pos": {
+          "x": -1570,
+          "y": 970
+        }
+      },
+      {
+        "type": "obstacle:clock",
+        "pos": {
+          "x": -1465,
+          "y": 1145
+        }
+      },
+      {
+        "type": "obstacle:clock",
+        "pos": {
+          "x": -1610,
+          "y": 535
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": -1810,
+          "y": 310
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": -1700,
+          "y": 500
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": -1530,
+          "y": 1055
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": -1560,
+          "y": 615
+        }
+      },
+      {
+        "type": "obstacle:clock",
+        "pos": {
+          "x": -1505,
+          "y": 730
+        }
+      },
+      {
+        "type": "obstacle:pillow",
+        "pos": {
+          "x": -1670,
+          "y": -535
+        }
+      },
+      {
+        "type": "obstacle:pillow",
+        "pos": {
+          "x": -1635,
+          "y": -655
+        }
+      },
+      {
+        "type": "obstacle:teapot",
+        "pos": {
+          "x": -1365,
+          "y": -1085
+        }
+      },
+      {
+        "type": "obstacle:teapot",
+        "pos": {
+          "x": -1295,
+          "y": -1185
+        }
+      },
+      {
+        "type": "obstacle:cup",
+        "pos": {
+          "x": -870,
+          "y": -1520
+        }
+      },
+      {
+        "type": "obstacle:cup",
+        "pos": {
+          "x": -760,
+          "y": -1570
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": -230,
+          "y": -1705
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": -85,
+          "y": -1735
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": 255,
+          "y": -1725
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": 400,
+          "y": -1675
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": 1000,
+          "y": -1400
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": 1115,
+          "y": -1300
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": 1330,
+          "y": -1070
+        }
+      },
+      {
+        "type": "obstacle:toaster",
+        "pos": {
+          "x": 1425,
+          "y": -970
+        }
+      },
+      {
+        "type": "obstacle:cup",
+        "pos": {
+          "x": 1320,
+          "y": 755
+        }
+      },
+      {
+        "type": "obstacle:cup",
+        "pos": {
+          "x": 1470,
+          "y": 1310
+        }
+      },
+      {
+        "type": "obstacle:teapot",
+        "pos": {
+          "x": -395,
+          "y": 1470
+        }
+      },
+      {
+        "type": "obstacle:teapot",
+        "pos": {
+          "x": -300,
+          "y": 1500
+        }
+      },
+      {
+        "type": "obstacle:teapot",
+        "pos": {
+          "x": -360,
+          "y": 1505
+        }
+      },
+      {
+        "type": "obstacle:pillow",
+        "pos": {
+          "x": -1715,
+          "y": -975
+        }
+      },
+      {
+        "type": "obstacle:pillow",
+        "pos": {
+          "x": -1325,
+          "y": -755
+        }
+      },
+      {
+        "type": "obstacle:teapot",
+        "pos": {
+          "x": -1275,
+          "y": -1500
+        }
+      },
+      {
+        "type": "obstacle:teapot",
+        "pos": {
+          "x": -965,
+          "y": -1175
+        }
+      },
+      {
+        "type": "obstacle:cup",
+        "pos": {
+          "x": -610,
+          "y": -1895
+        }
+      },
+      {
+        "type": "obstacle:cup",
+        "pos": {
+          "x": -475,
+          "y": -1455
+        }
+      }
+    ],
+    "keys": [
+      {
+        "x": 1035,
+        "y": 1430
+      },
+      {
+        "x": -1725,
+        "y": -190
+      },
+      {
+        "x": 535,
+        "y": 1430
+      },
+      {
+        "x": 410,
+        "y": 1475
+      },
+      {
+        "x": 305,
+        "y": 1495
+      },
+      {
+        "x": 175,
+        "y": 1505
+      },
+      {
+        "x": 100,
+        "y": 1520
+      },
+      {
+        "x": -10,
+        "y": 1530
+      },
+      {
+        "x": -1455,
+        "y": 485
+      },
+      {
+        "x": -1410,
+        "y": 575
+      },
+      {
+        "x": -1370,
+        "y": 670
+      },
+      {
+        "x": 625,
+        "y": -1385
+      }
+    ]
   },
   {
     "player": {
